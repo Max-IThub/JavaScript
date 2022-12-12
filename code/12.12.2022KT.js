3ac5b2a4023c136f0c6c1882683ba0ca27610457
Max <Nikolaev.max05@gmail.com>
2022-12-12 17:07:25 +0300

    new

--- code/12.12.2022KT.js
+++ code/12.12.2022KT.js
@@ -1,10 +1,52 @@
 let users = []
 
 function addUser(name, lastname, age) {
-    users.map(name, lastname, age => ({name: name, lastname: lastname, age: age}))
-    return user
+    users.push({
+        id: users.length + 1,
+        name: name,
+        lastname: lastname,
+        age: age
+    })
+    return users
 }
 
-addUser("Максим Николаев 18")
+function updateUser(id,name,lastname,age)
 
-console.log(users)
+{
+
+    for(let i = 0; i < users.length; i++)
+
+    {
+
+        if(users[i].id == id && typeof name == "string" && typeof lastname == "string" && typeof age == "number")
+
+        {
+            users[i].name = name
+            users[i].age = age
+            users[i].lastname = lastname
+            return users
+        }   
+    }
+    return 'Повторите попытку'
+}
+
+function deleteUser(id)
+{
+    for(let i = 0; i < users.length; i++)
+
+    {
+        if(users[i].id == id && typeof id == "number")
+
+        {
+            delete users[i]
+        }
+    }
+
+    return users
+}
+
+console.log(addUser('Николаев', 'Максим', 18))
+console.log(addUser('Васильев', 'Данил', 22))
+console.log(updateUser(1,'Максимов', 'Николай', 17))
+console.log(deleteUser(2))
+console.log(addUser('Бодрова', 'Анастасия', 16))
