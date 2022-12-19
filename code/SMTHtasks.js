bdf95bc78e7b0518e461372c952cb8de5ea8ce4c
Max <Nikolaev.max05@gmail.com>
2022-12-19 10:35:46 +0300

    new commit

--- code/SMTHtasks.js
+++ code/SMTHtasks.js
@@ -1,1 +1,10 @@
-SMTHtasks.js
+function factorial30(number) {
+	if(number >= 0 && number <= 30){
+		  
+
+	}return (number != 1) ? number * factorial(number - 1) : 1;
+	// else{
+	// 	console.log(`Вы ввели неверное значение переменной $number`)
+	// }
+}
+factorial30(20)
