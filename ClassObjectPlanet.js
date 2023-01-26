982951339fc6d9e6abf220abfb655adfcf7c548b
Max <Nikolaev.max05@gmail.com>
2023-01-26 11:53:39 +0300

    new

---
+++ ClassObjectPlanet.js
@@ -0,0 +1,11 @@
+class Family{
+    constructor(name, surname, iLoveYou){
+        this.name = name,
+        this.age = surname,
+        this.iLoveYou = iLoveYou
+    }
+}
+
+let Earth = new Planet(30000,true,false)
+
+console.log(Earth)  
